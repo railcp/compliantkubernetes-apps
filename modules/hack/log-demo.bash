fb58672c9069111c0cf0c22c7e32515fff8348d1
Simon Kollberg <simonkollberg@gmail.com>
2025-02-12 17:03:42 +0100

    WIP

--- modules/hack/log-demo.bash
+++ modules/hack/log-demo.bash
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+if [ "$(kind get clusters | wc -l)" -ne "1" ]; then
+  echo "number of kind clusters is not exactly 1" >&2
+  exit 1
+fi
+
 helmfile -e service_cluster apply -l name=crossplane-packages --include-transitive-needs
 
 kubectl wait --for condition=Healthy=true providers.pkg.crossplane.io/provider-helm --timeout 120s
@@ -9,5 +14,13 @@ kubectl wait --for condition=Healthy=true providers.pkg.crossplane.io/provider-k
 kubectl wait --for condition=Healthy=true functions.pkg.crossplane.io/function-patch-and-transform --timeout 120s
 kubectl wait --for condition=Healthy=true functions.pkg.crossplane.io/function-environment-configs --timeout 120s
 kubectl wait --for condition=Healthy=true configurations.pkg.crossplane.io/opensearch --timeout 120s
+
+kubectl -n crossplane-system create secret generic wc-kubeconfig \
+  --from-literal=value="$(
+    kind get kubeconfig --name "$(kind get clusters)" |
+      sed 's|server:.*|server: https://kubernetes.default|'
+  )" \
+  -o yaml --dry-run=client |
+  kubectl apply -f -
 
 helmfile -e service_cluster apply -l name=module-opensearch --include-transitive-needs
