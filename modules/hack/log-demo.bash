561707b158a0f72f69f5c48e46752e7503eb3aab
Simon Kollberg <simonkollberg@gmail.com>
2025-02-14 17:06:06 +0100

    WIP

--- modules/hack/log-demo.bash
+++ modules/hack/log-demo.bash
@@ -2,25 +2,9 @@
 
 set -euo pipefail
 
-if [ "$(kind get clusters | wc -l)" -ne "1" ]; then
-  echo "number of kind clusters is not exactly 1" >&2
-  exit 1
-fi
+here="$(dirname "$(readlink -f "$0")")"
 
-helmfile -e service_cluster apply -l name=crossplane-packages --include-transitive-needs
+root="${here}/../.."
 
-kubectl wait --for condition=Healthy=true providers.pkg.crossplane.io/provider-helm --timeout 120s
-kubectl wait --for condition=Healthy=true providers.pkg.crossplane.io/provider-kubernetes --timeout 120s
-kubectl wait --for condition=Healthy=true functions.pkg.crossplane.io/function-patch-and-transform --timeout 120s
-kubectl wait --for condition=Healthy=true functions.pkg.crossplane.io/function-environment-configs --timeout 120s
-kubectl wait --for condition=Healthy=true configurations.pkg.crossplane.io/opensearch --timeout 120s
-
-kubectl -n crossplane-system create secret generic wc-kubeconfig \
-  --from-literal=value="$(
-    kind get kubeconfig --name "$(kind get clusters)" |
-      sed 's|server:.*|server: https://kubernetes.default|'
-  )" \
-  -o yaml --dry-run=client |
-  kubectl apply -f -
-
-helmfile -e service_cluster apply -l name=module-opensearch --include-transitive-needs
+# "${root}/bin/ck8s" ops helmfile sc diff -l name=module-opensearch --include-transitive-needs --output simple
+"${root}/bin/ck8s" ops helmfile sc apply -l name=module-opensearch --include-transitive-needs
