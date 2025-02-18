0be4a67538bd2ca66bce2c1f48798dda840026d2
Simon Kollberg <simonkollberg@gmail.com>
2025-02-18 17:23:55 +0100

    WIP

--- modules/hack/log-demo.bash
+++ modules/hack/log-demo.bash
@@ -6,5 +6,12 @@ here="$(dirname "$(readlink -f "$0")")"
 
 root="${here}/../.."
 
+# "${root}/bin/ck8s" ops helmfile sc diff -l name=ingress-nginx --include-transitive-needs --output simple
+# "${root}/bin/ck8s" ops helmfile sc apply -l name=ingress-nginx --include-transitive-needs
+
+# "${root}/bin/ck8s" ops helmfile sc diff -l name=module-opensearch --include-transitive-needs --show-secrets
 # "${root}/bin/ck8s" ops helmfile sc diff -l name=module-opensearch --include-transitive-needs --output simple
 "${root}/bin/ck8s" ops helmfile sc apply -l name=module-opensearch --include-transitive-needs
+
+# "${root}/bin/ck8s" ops helmfile wc diff -l name=module-fluentd-forwarder --include-transitive-needs --output simple
+# "${root}/bin/ck8s" ops helmfile wc apply -l name=module-fluentd-forwarder --include-transitive-needs
